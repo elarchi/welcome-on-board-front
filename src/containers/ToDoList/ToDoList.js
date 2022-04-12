3f8c1a4cf1e4bf74638138de715a135e28e95aab
elarchi <emilie.leury@gmail.com>
2022-04-12 19:27:37 +0100

    headband creation

--- src/containers/ToDoList/ToDoList.js
+++ src/containers/ToDoList/ToDoList.js
@@ -3,7 +3,14 @@ import "./todolist.scss";
 
 const ToDoList = ({ setHeaderState }) => {
   setHeaderState(2);
-  return <div className="todolist__div">ToDoList Component</div>;
+  return (
+    <div className="todolist__div">
+      <div className="container__div headband__div ">
+        <h1>LA DATE DU JOUR</h1>
+        <button>ADD LIST</button>
+      </div>
+    </div>
+  );
 };
 
 export default ToDoList;
